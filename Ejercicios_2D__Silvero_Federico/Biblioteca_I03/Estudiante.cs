e8720b6044f0d620d33f9536efa7c24f56e718ee
fedesilveroutn <fedesilveromusico@gmail.com>
2022-08-25 10:33:13 -0300

    Actualizacion

---
+++ Ejercicios_2D__Silvero_Federico/Biblioteca_I03/Estudiante.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Text;
+
+namespace Biblioteca_I03
+{
+    public class Estudiante
+    {
+        private string apellido;
+        private string legajo;
+        private string nombre;
+        private int notaPrimerParcial;
+        private int notaSegundoParcial;
+        private static Random random;
+
+
+
+        static Estudiante() 
+        {
+            random = new Random();
+        }
+
+        public Estudiante(string nombre, string apellido, string legajo) 
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.legajo = legajo;
+        }
+
+        public void SetNotaPrimerParcial(int nota)
+        {
+            this.notaPrimerParcial = nota;
+        }
+
+        public void SetNotaSegundoParcial(int nota)
+        {
+            this.notaSegundoParcial = nota;
+        }
+
+
+        private float CalcularPromedio() 
+        {
+            return (this.notaPrimerParcial + this.notaSegundoParcial) / 2;
+        }
+
+
+
+        public double CalcularNotaFinal() 
+        {
+            double retorno = -1;
+            if(this.notaPrimerParcial >= 4 && this.notaSegundoParcial >= 4)
+            {
+                retorno = random.Next(6, 10);
+            }
+            return retorno;
+        }
+
+
+        public string Mostrar() 
+        {
+            StringBuilder infoEstudiante = new StringBuilder();
+            double notaFinal;
+
+            infoEstudiante.AppendLine($"{this.nombre} {this.apellido} | Legajo Nro: {this.legajo}");
+            infoEstudiante.AppendLine($"Primer parcial: {this.notaPrimerParcial} | Segundo parcial: {this.notaSegundoParcial}");
+            infoEstudiante.AppendLine($"Promedio: {this.CalcularPromedio()}");
+
+            notaFinal = CalcularNotaFinal();
+            if(notaFinal != -1)
+            {
+                infoEstudiante.AppendLine($"Nota final: {notaFinal}");
+            }
+            else
+            {
+                infoEstudiante.AppendLine("Alumno desaprobado");
+            }
+
+            return infoEstudiante.ToString();
+        }
+
+
+
+
+       
+
+        
+
+
+        
+
+
+    }
+}
---
+++ Ejercicios_2D__Silvero_Federico/POO_I03/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Biblioteca_I03;
+
+namespace POO_I03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Estudiante estudiante1 = new Estudiante("Federico", "Silvero", "1000");
+            Estudiante estudiante2 = new Estudiante("Valentin", "Silvero", "1002");
+            Estudiante estudiante3 = new Estudiante("Constanza", "Arancio", "1003");
+
+            estudiante1.SetNotaPrimerParcial(10);
+            estudiante1.SetNotaSegundoParcial(9);
+
+            estudiante2.SetNotaPrimerParcial(8);
+            estudiante2.SetNotaSegundoParcial(10);
+
+            estudiante3.SetNotaPrimerParcial(3);
+            estudiante3.SetNotaSegundoParcial(2);
+
+
+            Console.WriteLine(estudiante1.Mostrar());
+            Console.WriteLine(estudiante2.Mostrar());
+            Console.WriteLine(estudiante3.Mostrar()); 
+        }
+    }
+}
